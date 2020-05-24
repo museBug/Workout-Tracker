bd9b66f86a5e3a2246c7114a8bcc255f8aedc026
Regina Musovirova <musovirova@yahoo.com>
2020-05-24 15:46:24 -0400

    fix bug in stats.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,8 @@
-const Workout = require("../models/workout.js")
+const Workout = require("./models/workout.js")
 
 module.exports = function (app) {
 
-    app.get("/api/workouts", function (req, res) {
+    app.get("../api/workouts", function (req, res) {
         Workout.find()
             .then(data => {
                 res.json(data)
@@ -12,7 +12,7 @@ module.exports = function (app) {
             })
     });
 
-    app.post("/api/workouts", function (req, res) {
+    app.post("../api/workouts", function (req, res) {
         Workout.create({})
             .then(data => res.json(data))
             .catch(err => {
@@ -21,16 +21,43 @@ module.exports = function (app) {
             })
     });
 
-    app.put("/api/workouts/:id", ({ body, params }, res) => {
-        Workout.findByIdAndUpdate(
-            params.id,
-            { $push: { exercises: body } },
-            { new: true, runValidators: true }
-        )
-            .then(data => res.json(data))
-            .catch(err => {
-                console.log("err", err)
-                res.json(err)
-            })
-    });
+    //Put request
+
+    app.put("../api/workouts/:id", (req,res) => {
+
+        let urlData = req.params;
+        let data = req.body;
+           Workout.updateOne( {_id: urlData.id }, {$push: {exercises:  [
+            {
+            "type" : data.type,
+            "name" : data.name,
+            "duration" : data.duration,
+            "distance" : data.distance,
+            "weight" : data.weight,
+            "reps" : data.reps,
+            "sets" : data.sets
+            }
+          ] 
+        }}).then(dbUpdate => {
+          res.json(dbUpdate);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+        
+        });
+
+  
+        //Get Range Request
+
+        app.get("/api/workouts/range", (req,res) => {
+        Workout.find({})
+        .then(data => {
+        res.json(data);
+        })
+        .catch(err => {
+        res.json(err);
+        });
+        });
+
 }
